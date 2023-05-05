944cc91d070c2f22e21636336c8bdc4a195f8034
Vitaliy Dorofeev <91981187+VitaliyDorofeev@users.noreply.github.com>
2023-05-05 12:21:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+	//options here
+	scrollHorizontally: true,
+	offsetSections: true,
+	scrollOverflow: false,
+	loopBottom: true,
+	normalScrollElements: '.subtitle--mod',
+});
